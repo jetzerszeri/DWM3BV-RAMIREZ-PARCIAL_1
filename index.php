4030ebd5726d561abaf7498866aa19e15b26a838
jetzerszeri <jetzers.2@gmail.com>
2023-05-19 14:21:54 -0400

    feat: add productos section with each card

--- index.php
+++ index.php
@@ -32,18 +32,64 @@
 
     <!-- main del home page -->
 
-        <div class="homebanner">
+        <!-- <div class="homebanner">
             <div>
                 <h1>Bienvenido <span>al paraíso de los libros</span></h1>
                 <p>“Un escritor solo comienza un libro, un lector lo termina”</p>
                 <a href="#" class="btn">Explorar catálogo</a>
             </div>
-        </div>
+        </div> -->
 
 
 
     
+    <!-- main de la pagina de todos los productos -->
+    <main>
+        <header>
+            <p><a href="#">Inicio</a> > <a href="#">Todos los libros</a></p>
+            <h1><span>Explora</span> Todos los libros</h1>
+        <header>
 
+        <div>
+            <ul class="listadelibros">
+                <li>
+                    <img src="assets/img/hungergames1.jpeg" alt="Tapa del libro Los juegos del hambre">
+                    <div>
+                        <ul>
+                            <li><h2>Los juegos<h2></li>
+                            <li>Suzanne Collins</li>
+                            <li>$10.98 US</li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <img src="assets/img/hungergames1.jpeg" alt="Tapa del libro Los juegos del hambre">
+                    <div>
+                        <ul>
+                            <li><h2>Los juegos del hambre<h2></li>
+                            <li>Suzanne Collins</li>
+                            <li>$10.98 US</li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <img src="assets/img/hungergames1.jpeg" alt="Tapa del libro Los juegos del hambre">
+                    <div>
+                        <ul>
+                            <li><h2>Los juegos del hambre<h2></li>
+                            <li>Suzanne Collins</li>
+                            <li>$10.98 US</li>
+                        </ul>
+                    </div>
+                </li>
+
+
+
+
+            </ul>
+        </div>
+
+    </main>
     
     <footer>
         <ul>
