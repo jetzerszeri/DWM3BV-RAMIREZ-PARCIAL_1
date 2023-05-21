726d66b7c6ab5356c8caea2b40ccfdab5ca02b79
jetzerszeri <jetzers.2@gmail.com>
2023-05-21 18:38:17 -0400

    feat: add 'detalles de producto' section

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
     
     <!-- main de la pagina de todos los productos -->
-    <main>
+    <!-- <main>
         <header>
             <p class="breadcumbs"><a href="#">Inicio</a> > <a href="#">Todos los libros</a></p>
             <h1><span>Explora</span> Todos los libros</h1>
@@ -89,11 +89,11 @@
             </ul>
         </div>
 
-    </main>
+    </main> -->
 
 
     <!-- main de la pagina de categorias -->   
-    <main>
+    <!-- <main>
 
         <header>
             <p class="breadcumbs"><a href="#">Inicio</a> > <a href="#">Categorías</a></p>
@@ -151,20 +151,49 @@
         </div>
 
 
+    </main> -->
+
+
+    <!-- main de la pagina de detalles de producto -->   
+    <main>
+
+            <p class="breadcumbs"><a href="#">Inicio</a> > <a href="#">Categorías</a> > <a href="#">Distopía</a> > Los juegos del hambre</p>
+
+            <div class="detalles">
+                <img src="assets/img/hungergames1.jpeg" alt="Tapa del libro Los juegos del hambre">
+                <div>
+                    <div>
+                    <p>Suzanne Collins</p>
+                    <h1>Los juegos del hambre</h1>
+                    <p>Género: Distopía</p>
+                    <p>$10.00 usd</p>
+                    <button>Agregar al carrito</button>
+                    </div>
+
+                    <h2>Descripción</h2>
+                    <p>El mundo está observando. Ganar significa fama y riqueza. Perder significa una muerte segura.</p>
+                    <p>¡Que empiecen los septuagésimo cuartos juegos del hambre!</p>
+                    <p>En una oscura versión del futuro próximo, doce chicos y doce chicas se ven obligados a participar en un reality show llamado Los Juegos del Hambre. Solo hay una regla: matar o morir.</p>
+                    <p>Cuando Katniss Everdeen, una joven de dieciséis años se presenta voluntaria para ocupar el lugar de su hermana en los juegos, lo entiende como una condena a muerte. Sin embargo, Katniss ya ha visto la muerte de cerca y la supervivencia forma parte de su naturaleza.</p>
+
+                    <h2>Detalles del producto</h2>
+                    <ul>
+                        <li><span>Publicación:</span> 15 Marzo 2012</li>
+                        <li><span>Editorial:</span> Lectorum Publications, Inc.</li>
+                        <li><span>Idioma:</span> Español</li>
+                        <li><span>Páginas:</span> 400</li>
+                    </ul>
+
+                </div>
+            </div>
+
+
+
+
+
+
+
     </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
