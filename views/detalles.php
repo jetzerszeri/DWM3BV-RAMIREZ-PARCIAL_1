9fdf0e6bdbdf1d24f75e93752468c1b28519cbaa
jetzerszeri <jetzers.2@gmail.com>
2023-05-21 22:45:09 -0400

    refactor: move detalles info into variables

--- views/detalles.php
+++ views/detalles.php
@@ -1,30 +1,41 @@
+<?php
+    $libro = "1984";
+   $autor= "George Orwell";
+    $genero= "ficcion";
+    $descripcion= "1984 es una novela distópica que presenta una sociedad totalitaria controlada por el Gran Hermano. Winston Smith, el protagonista, lucha por rebelarse contra el sistema opresivo y por preservar su individualidad.";
+    $publicacion= "1949";
+    $editorial= "Secker & Warburg";
+    $idioma= "Inglés";
+    $paginas= 328;
+    $imagen= "assets/img/1984.jpg";
+    $id= 1;
+    $precio = 11.50;
+?>
+
 <main>
 
 <p class="breadcumbs"><a href="index.php?seccion=home">Inicio</a> > <a href="#">Categorías</a> > <a href="#">Distopía</a> > Los juegos del hambre</p>
 
 <div class="detalles">
-    <img src="assets/img/hungergames1.jpeg" alt="Tapa del libro Los juegos del hambre">
+    <img src="<?= $imagen ?>" alt="Tapa del libro <?= $libro ?>">
     <div>
         <div>
-            <p>Suzanne Collins</p>
-            <h1>Los juegos del hambre</h1>
-            <p>Género: Distopía</p>
-            <p>$10.00 usd</p>
+            <p><?= $autor ?></p>
+            <h1><?= $libro ?></h1>
+            <p>Género: <?= $genero ?></p>
+            <p>$<?= $precio ?> usd</p>
             <button>Agregar al carrito</button>
         </div>
 
         <h2>Descripción</h2>
-        <p>El mundo está observando. Ganar significa fama y riqueza. Perder significa una muerte segura.</p>
-        <p>¡Que empiecen los septuagésimo cuartos juegos del hambre!</p>
-        <p>En una oscura versión del futuro próximo, doce chicos y doce chicas se ven obligados a participar en un reality show llamado Los Juegos del Hambre. Solo hay una regla: matar o morir.</p>
-        <p>Cuando Katniss Everdeen, una joven de dieciséis años se presenta voluntaria para ocupar el lugar de su hermana en los juegos, lo entiende como una condena a muerte. Sin embargo, Katniss ya ha visto la muerte de cerca y la supervivencia forma parte de su naturaleza.</p>
+        <p><?= $descripcion ?></p>
 
         <h2>Detalles del producto</h2>
         <ul>
-            <li><span>Publicación:</span> 15 Marzo 2012</li>
-            <li><span>Editorial:</span> Lectorum Publications, Inc.</li>
-            <li><span>Idioma:</span> Español</li>
-            <li><span>Páginas:</span> 400</li>
+            <li><span>Publicación:</span> <?= $publicacion ?></li>
+            <li><span>Editorial:</span> <?= $editorial ?></li>
+            <li><span>Idioma:</span> <?= $idioma ?></li>
+            <li><span>Páginas:</span> <?= $paginas ?></li>
         </ul>
 
     </div>
