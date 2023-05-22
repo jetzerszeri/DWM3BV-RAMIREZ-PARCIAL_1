a14bd189a55a4a90095fabd1f1ff5cae1e5ac5d7
jetzerszeri <jetzers.2@gmail.com>
2023-05-21 20:44:01 -0400

    refactor: move header content to a partial

--- index.php
+++ index.php
@@ -1,34 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Olympbooks</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    
-    <nav>
-        <div class="navbar">
-        <div>Olympbooks</div>
-        <a href="#" class="toggle-button">
-            <span class="bar"></span>
-            <span class="bar"></span>
-            <span class="bar"></span>
-        </a>
-        <div class="navbar-links">
-            <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Productos</a></li>
-            <li><a href="#">Categorías</a></li>
-            <li><a href="#">Contacto</a></li>
-            <li><a href="#">Datos</a></li>
-            </ul>
-        </div>
-        </div>
-    </nav>
-
+<?php include('partials/header.php')?>
 
     <!-- main del home page -->
     <!-- <div class="homebanner">
@@ -226,7 +196,6 @@
 
     <!-- main de la pagina de contacto parte 2 -->
     <main>
-
         <div class="formularioenviado">
             <p>¡Mensaje enviado exitosamente!</p>
             <h1>Muchas gracias por contactarnos, Jetzer</h1>
---
+++ partials/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olympbooks</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <nav>
+        <div class="navbar">
+        <div>Olympbooks</div>
+        <a href="#" class="toggle-button">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </a>
+        <div class="navbar-links">
+            <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Productos</a></li>
+            <li><a href="#">Categorías</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li><a href="#">Datos</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
